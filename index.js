2e666435097edfd2b02c3740c77bb034e17d2041
theopappas17 <theopappas17@gmail.com>
2022-06-07 22:15:18 -0400

    Index.js handles prompts as desired

--- index.js
+++ index.js
@@ -6,23 +6,23 @@ const Employee = require("./Classes/Employee.js");
 inquirer.prompt([
   {
     type: "input",
-    name: empName,
+    name: "empName",
     message: "What is your name?",
   },
   {
     type: "list",
-    name: empPosition,
+    name: "empPosition",
     message: "What is you position in the company",
     choices: ["Manager", "Engineer", "Intern"],
   },
   {
     type: "input",
-    name: empNum,
+    name: "empNum",
     message: "What is your employee ID number?",
   },
   {
     type: "input",
-    name: empNum,
+    name: "empNum",
     message: "What is your company email?",
   },
 ]);
