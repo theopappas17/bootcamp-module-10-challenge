7d7910528a3ae1887aa09a8f70653ea528c05126
theopappas17 <theopappas17@gmail.com>
2022-06-07 23:57:37 -0400

    Final commit before assignment due. Did not finish.

--- generateHtml.js
+++ generateHtml.js
@@ -1,3 +1,6 @@
+// Need the File System module built into Node.js
 const fs = require('fs');
 
-module.exports =
+module.exports = ({empName, empPosition, empNum, email}) => {
+    
+}
--- index.js
+++ index.js
@@ -3,26 +3,30 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 const Employee = require("./Classes/Employee.js");
 
-inquirer.prompt([
-  {
-    type: "input",
-    name: "empName",
-    message: "What is your name?",
-  },
-  {
-    type: "list",
-    name: "empPosition",
-    message: "What is you position in the company",
-    choices: ["Manager", "Engineer", "Intern"],
-  },
-  {
-    type: "input",
-    name: "empNum",
-    message: "What is your employee ID number?",
-  },
-  {
-    type: "input",
-    name: "empNum",
-    message: "What is your company email?",
-  },
-]);
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "empName",
+      message: "What is your name?",
+    },
+    {
+      type: "list",
+      name: "empPosition",
+      message: "What is you position in the company",
+      choices: ["Manager", "Engineer", "Intern"],
+    },
+    {
+      type: "input",
+      name: "empNum",
+      message: "What is your employee ID number?",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "What is your company email?",
+    },
+  ])
+  .then((answers) => {
+    generateClasses(answers);
+  });
